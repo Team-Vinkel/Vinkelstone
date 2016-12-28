4a14270e3c7cc48f26360cf31c886a91273a4c16
Def4l71diot <lubeto3099@gmail.com>
2016-12-28 21:42:27 +0200

    Some more work on the kinvey part

--- src/app/shared/kinvey/kinvey.config.ts
+++ src/app/shared/kinvey/kinvey.config.ts
@@ -6,8 +6,27 @@ export class KinveyConfig {
     private _APP_MASTER = 'd580dbb3e2df4867b3423c293a1edc94';
     private _AUTHORIZATION_STRING = `${this._APP_ID}:${this._APP_SECRET}`;
     private _AUTHORIZATION_STRING_MASTER = `${this._APP_ID}:${this._APP_MASTER}`;
-    private _AUTH_HEADER_COMMON: string;
-    private _AUTH_HEADER_USERS: string;
+    private _AUTH_STRING_COMMON: string;
+    private _AUTH_STRING_USERS: string;
+    private _AUTHORIZATION_HEADER_NAME = 'Authorization';
+    private _AUTHORIZATION_TYPE = 'Basic';
+    private _BASE_URL = 'https://baas.kinvey.com/';
+    private _COLLECTIONS_URL = `${this._BASE_URL}appdata/${this.APP_ID}/`;
+
+    public AUTH_HEADER_COMMON = {
+        name: this._AUTHORIZATION_HEADER_NAME,
+        value: `${this._AUTHORIZATION_TYPE} ${this._AUTH_STRING_COMMON}`
+    };
+
+    public AUTH_HEADER_USERS = {
+        name: this._AUTHORIZATION_HEADER_NAME,
+        value: `${this._AUTHORIZATION_TYPE} ${this._AUTH_STRING_USERS}`
+    };
+
+    public CONTENT_TYPE_HEADER = {
+        name: `Content-Type`,
+        value: `application/json`
+    };
 
     constructor() {
         this.loadBase64AuthKeys();
@@ -17,16 +36,12 @@ export class KinveyConfig {
         return this._APP_ID;
     }
 
-    public get AUTH_HEADER_COMMON(){
-        return this._AUTH_HEADER_COMMON;
-    }
-
-    public get AUTH_HEADER_USERS(){
-        return this._AUTH_HEADER_USERS;
+    public getCollectionUrl(collectionName: string, filter?: string) {
+        return `${this._COLLECTIONS_URL}${collectionName}/${filter ? '?query=' + filter : ''}`;
     }
 
     private loadBase64AuthKeys(): void {
-        this._AUTH_HEADER_COMMON = Crypto.encryptToBase64(this._AUTHORIZATION_STRING_MASTER);
-        this._AUTH_HEADER_USERS = Crypto.encryptToBase64(this._AUTHORIZATION_STRING);
+        this._AUTH_STRING_COMMON = Crypto.encryptToBase64(this._AUTHORIZATION_STRING_MASTER);
+        this._AUTH_STRING_USERS = Crypto.encryptToBase64(this._AUTHORIZATION_STRING);
     }
 }
--- src/app/shared/kinvey/kinvey.service.ts
+++ src/app/shared/kinvey/kinvey.service.ts
@@ -5,16 +5,31 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class KinveyService {
-    // TEST_URL = `https://baas.kinvey.com/appdata/${this.APP_ID}`;
 
     constructor(private _config: KinveyConfig, private _http: Http) {
     }
 
-    testKinvey() {
-        // let headers: Headers = new Headers();
+    getCollection(collectionName: string, filter?: string) {
+        let url = this._config.getCollectionUrl(collectionName, filter);
+        let auth = this._config.AUTH_HEADER_COMMON;
 
-        // headers.append('Authorization', `Basic ${this.AUTORIZATION_HEADER_MAIN}`);
-        // return this._http.get(this.TEST_URL, { headers })
-        //     .map(res => res.json());
+        let headers = new Headers();
+        headers.append(auth.name, auth.value);
+
+        return this._http.get(url, { headers })
+            .map(res => res.json());
+    }
+
+    postCollection(collectionName: string, body: Object) {
+        let url = this._config.getCollectionUrl(collectionName);
+        let auth = this._config.AUTH_HEADER_COMMON;
+        let contentType = this._config.CONTENT_TYPE_HEADER;
+
+        let headers = new Headers();
+        headers.append(auth.name, auth.value);
+        headers.append(contentType.name, contentType.value);
+
+        return this._http.post(url, JSON.stringify(body), { headers })
+            .map(res => res.json());
     }
 }
