e976f26fc5b0983327ceec575f0a9fa71a74a129
ultron1251 <alexander.yankov1173@abv.bg>
2016-12-28 22:59:57 +0200

    added bootstrap and a crappy navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { HomeComponent } from './home/home.component';
         HttpModule,
         RouterModule.forRoot([
             {
-                path: '',
+                path: 'home',
                 component: HomeComponent
             }
         ])
