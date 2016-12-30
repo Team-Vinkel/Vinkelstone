5dddf5f09e62868c4dafe498fc10cecd72432864
Def4l71diot <lubeto3099@gmail.com>
2016-12-30 17:55:59 +0200

    We can now create all kinds of cards

--- src/app/cards/create-card/create-weapon/create-weapon.component.ts
+++ src/app/cards/create-card/create-weapon/create-weapon.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CardService } from './../../shared/card.service';
+import { ICard } from '../../shared/card';
+import { CardType } from '../../shared/enums/card-type';
+
 @Component({
   selector: 'app-create-weapon',
   templateUrl: './create-weapon.component.html',
   styleUrls: ['./create-weapon.component.css']
 })
 export class CreateWeaponComponent implements OnInit {
+  card: ICard;
 
-  constructor() { }
+  constructor(private _cardService: CardService) { }
 
   ngOnInit() {
+    this.card = { type: CardType.Weapon };
   }
 
+  createCard() {
+    this._cardService.createCard(this.card)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      );
+  }
 }
