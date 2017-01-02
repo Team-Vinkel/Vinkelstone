b52920e6b4819e7f875d3e2a49edba59400ecec1
ultron1251 <alexander.yankov1173@abv.bg>
2017-01-03 01:37:18 +0200

    removed wrong imports

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CardsListComponent } from './cards-list/cards-list.component';
 import { CreateCardModule } from './create-card/create-card.module';
-import { CardsUserComponent } from './cards-user/cards-user.component';
 
 import { PipesModule } from './../pipes/pipes.module';
 import { CardViewComponent } from './card-view/card-view.component';
