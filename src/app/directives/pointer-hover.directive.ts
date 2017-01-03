81ff9681a1a32ad9207aa4e795fd44ca224862a9
Def4l71diot <lubeto3099@gmail.com>
2017-01-03 04:25:22 +0200

    Pointer hover directive

---
+++ src/app/directives/pointer-hover.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+const CURSOR_STYLES = [
+  'http://www.w3schools.com/cssref/smiley.gif',
+  'http://www.rw-designer.com/cursor-extern.php?id=76012',
+  'http://www.rw-designer.com/cursor-extern.php?id=76021',
+  'pointer'
+];
+@Directive({
+  selector: '[appPointerHover]'
+})
+export class PointerHoverDirective {
+
+  private _oldPictureUrl: string;
+
+  constructor(private _element: ElementRef) {
+    this._element.nativeElement.style.cursor = this._getRandomPointerStyle();
+  }
+
+  private _getRandomPointerStyle() {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    let random = getRandomInt(0, CURSOR_STYLES.length - 1);
+    return `url(${CURSOR_STYLES[random]})`;
+  }
+}
